226d52b32a4ad4f47fc8375729a653106510ac86
Keshav-goyal-1 <goyalkeshav896@gmail.com>
2025-01-24 11:25:53 +0530

    Adding the genAi functionality

--- src/Components/sideBar/Sidebar.jsx
+++ src/Components/sideBar/Sidebar.jsx
@@ -1,24 +1,37 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import './sidebar.css'
 import {assets} from '../../assets/assets'
+import { Context } from '../../context/context'
 const Sidebar = () => {
 
     const [extended,setExtended] = useState(false)
+    const {onSent,previousPrompts,setRecentPrompt,newChat} = useContext(Context)
+
+    const loadPrompt= async (prompt) => {
+        setRecentPrompt(prompt)
+        await onSent(prompt)
+    }
 
   return (
     <div className='Sidebar' >
         <div className="top">
             <img onClick={()=>setExtended(prev=>!prev)} className='menu' src={assets.menu_icon} alt="" />
-            <div className="new_chat">
+            <div onClick={()=>newChat()} className="new_chat">
                 <img src={assets.plus_icon} alt="" />
                {extended?<p>New Chat</p>:null} 
             </div>
             {extended?<div className="recent">
                 <div className="recent_title">Recent</div>
-                <div className="recent_entry">
+                {previousPrompts.map((item,index)=>{
+                    return(
+                <div onClick={()=>loadPrompt(item)} className="recent_entry">
                     <img src={assets.message_icon} alt="" />
-                    <p>What is react..</p>
+                    <p>{item.slice(0,18)}..</p>
                 </div>
+
+                    )
+                }
+                )}
             </div> :null}
                  
         </div>
--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -4,6 +4,7 @@ import run from "../config/gemini.js";
 export const Context = createContext();
 
 
+
 const ContextProvider = (props)=>{
 
     const [input,setInput] = useState("");
@@ -13,8 +14,53 @@ const ContextProvider = (props)=>{
     const [loading, setLoading] = useState(false)
     const [resultData, setResultData] = useState("")
     
+    const delayPara = (index,nextWord)=>{
+        setTimeout(function () {
+            setResultData(prev=>prev+nextWord)
+        },index*75)
+    }
+
+    const newChat = ()=>{
+        setLoading(false)
+        setShowResult(false)
+    }
+
     const onSent = async (prompt)=>{
-        await run(prompt)
+
+        setResultData("")
+        setLoading(true)
+        setShowResult(true)
+        let response ;
+        if(prompt !== undefined){
+            response = await run(prompt);
+            setRecentPrompt(prompt)
+        }
+        else{
+            setPreviousPrompts(prev=>[...prev,input])
+            response = await run(input)
+            setRecentPrompt(input)
+        }
+        let responseArray = response.split("**");
+        let newResponse = "";
+        for(let i=0; i<responseArray.length; i++)
+        {
+            if(i === 0 || i%2!== 1){
+                newResponse += responseArray[i];
+            }
+            else{
+                newResponse += "<b>" + responseArray[i] +"</b>";
+            }
+        }
+
+        let newResponse2 = newResponse.split("*").join("</br>");
+        let newResponseArray = newResponse2.split(" ");
+        for(let i=0;i<newResponseArray.length;i++)
+        {
+            const nextWord = newResponseArray[i];
+            delayPara(i,nextWord+" ");
+        }
+        setLoading(false)
+        setInput("")
     }
     
     // useEffect(() => {
@@ -22,7 +68,17 @@ const ContextProvider = (props)=>{
     // }, []); 
     
     const contextValue ={
-        
+        previousPrompts,
+        setPreviousPrompts, 
+        onSent, 
+        setRecentPrompt,
+        recentPrompt,
+        showResult,
+        loading,
+        resultData,
+        input,
+        setInput,
+        newChat
     }
     
     return(
